c5a25db158baeae8fef3ced5a51a295f35d6002b
Chloe <2106@holbertonschool.com>
2020-12-21 17:44:39 +0000

    fixing

--- run.c
+++ run.c
@@ -14,29 +14,28 @@ var_1 vari;
          {"pall", pall},
          {NULL, NULL},
      };
-     for (j = 0; j < 3; j++)
+     for (j = 0; j < 4; j++)
      {
          if(strcmp(funcs[j].opcode, refer[0]) == 0)
          {
-             
-             /*if (isalpha(refer[1]) == 0)
-             {
-                 fprintf(stderr,"L%d: usage: push integer\n", vari.line_number);
-                 fclose(vari.fil);
-                 f_list(vari.mystack);
-                 exit(EXIT_FAILURE);
-             }*/
              vari.num = refer[1];
              funcs[j].f(&(vari.mystack), vari.line_number);
              return;
          }
-        
-         else if (strcmp(funcs[j].opcode, refer[0]) != 0)
-         {
-             fprintf(stderr, "L%u: unknown instruction %s\n", vari.line_number, refer[0]);
-             fclose(vari.fil);
-             f_list(vari.mystack);
-             exit(EXIT_FAILURE);
-         }
+
+        else if (j == 2)
+        {
+            fprintf(stderr, "L%u: unknown instruction %s\n", vari.line_number, refer[0]);
+            fclose(vari.fil);
+            f_list(vari.mystack);
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            fprintf(stderr, "L%u: unknown instruction %s\n", vari.line_number, refer[0]);
+            fclose(vari.fil);
+            f_list(vari.mystack);
+            exit(EXIT_FAILURE);
+        }
     }
  }
